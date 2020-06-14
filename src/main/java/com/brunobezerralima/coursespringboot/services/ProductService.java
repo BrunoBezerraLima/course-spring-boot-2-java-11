2bc2b4c38d8de2ff7d3bb2119f9bc709012d0c00
Bruno Bezerra Lima <brunobezerralima@gmail.com>
2020-06-13 22:22:33 -0300

    Product

---
+++ src/main/java/com/brunobezerralima/coursespringboot/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.brunobezerralima.coursespringboot.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.brunobezerralima.coursespringboot.entities.Product;
+import com.brunobezerralima.coursespringboot.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll(){
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+}
